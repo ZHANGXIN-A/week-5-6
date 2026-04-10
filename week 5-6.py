127d43f7cac17df925222e272b3ca1908c38728d
Xin Zhang <zx1972893936@ajou.ac.kr>
2026-04-10 22:40:49 +0900

    random

--- week 5-6.py
+++ week 5-6.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from xgboost import XGBClassifier
 
 df = pd.read_csv("titanic.csv")
 
